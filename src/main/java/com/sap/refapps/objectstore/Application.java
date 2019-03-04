a395b5d570cbe7be6b9f5d8553ea355627d2faeb
indusankar <i.sankar@sap.com>
2019-03-04 13:54:22 +0530

    skeletal app for tutorial

--- src/main/java/com/sap/refapps/objectstore/Application.java
+++ src/main/java/com/sap/refapps/objectstore/Application.java
@@ -1,17 +1,13 @@
 package com.sap.refapps.objectstore;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-
-import com.sap.refapps.objectstore.config.ObjectStoreContextInitializer;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		new SpringApplicationBuilder(Application.class)
-		.initializers(new ObjectStoreContextInitializer()).run(args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
-
